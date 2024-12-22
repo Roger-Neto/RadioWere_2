4c3188d1e20640956bf7b6ef89c849acc2c48145
José Rogério Neto <jose.rogerio.n@gmail.com>
2024-12-21 21:28:25 -0300

    Roger-Neto
    Implantação das novas paginas (by pizzade42queijos)

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import home, save, edit, update, delete
 
 urlpatterns = [
-    path('', home),
+    path('', home, name='home'),
     path('save/', save, name="save"),
     path('edit/<int:id>', edit, name='edit'),
     path('update/<int:id>', update, name='update'),
